d19cb177c41ee704f995776e594aeabc84d13dbe
Kanaru Sato <kanakanarunrun@gmail.com>
2023-04-08 00:52:09 +0900

    add command

--- cli/main.rs
+++ cli/main.rs
@@ -1,34 +1,56 @@
 use rsqloth_core::format_insert_queries;
-use std::{env, fs::OpenOptions, io::Read, process};
+use std::{env, fs::OpenOptions, io::Read, process, str::FromStr};
+use strum::IntoEnumIterator;
+use strum_macros::{Display, EnumIter, EnumString};
+
+#[derive(Debug, EnumString, EnumIter, Display)]
+enum Commands {
+    #[strum(serialize = "fmtinserts")]
+    FmtInserts,
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    for path in args.iter().skip(1) {
-        let mut f = OpenOptions::new()
-            .read(true)
-            .open(path)
-            .unwrap_or_else(|err| {
-                eprintln!("failed to open the file. error: {err}");
+    match Commands::from_str(&args[1]) {
+        Ok(Commands::FmtInserts) => {
+            let path = &args[2];
+
+            let mut f = OpenOptions::new()
+                .read(true)
+                .open(path)
+                .unwrap_or_else(|err| {
+                    eprintln!("failed to open the file. error: {err}");
+                    process::exit(1);
+                });
+
+            let mut sql = String::new();
+            f.read_to_string(&mut sql).unwrap_or_else(|err| {
+                eprintln!("failed to read the file. error: {err}");
                 process::exit(1);
             });
 
-        let mut sql = String::new();
-        f.read_to_string(&mut sql).unwrap_or_else(|err| {
-            eprintln!("failed to read the file. error: {err}");
+            let res = format_insert_queries(&sql);
+            match res {
+                Ok(res) => {
+                    println!("{res}");
+                    process::exit(0);
+                }
+                Err(err) => {
+                    eprintln!("failed to format. error: {err}");
+                    process::exit(1);
+                }
+            }
+        }
+        _ => {
+            let available_commands = Commands::iter()
+                .map(|command| command.to_string())
+                .collect::<Vec<_>>();
+            eprintln!(
+                "only the following commands are available: {:?}",
+                available_commands
+            );
             process::exit(1);
-        });
-
-        let res = format_insert_queries(&sql);
-        match res {
-            Ok(res) => {
-                println!("{res}");
-                process::exit(0);
-            }
-            Err(err) => {
-                eprintln!("failed to format. error: {err}");
-                process::exit(1);
-            }
         }
     }
 }
